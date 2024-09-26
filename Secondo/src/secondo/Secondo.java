f63cde0786ca8d54d62cf66d93be919efd0a6d79
Lorenzo Tripodi <157422066+Eltymus@users.noreply.github.com>
2024-09-26 14:05:55 +0200

    Add files via upload

---
+++ Secondo/src/secondo/Secondo.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package secondo;
+
+/**
+ *
+ * @author loren
+ */
+public class Secondo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // Questo comando stmapa una frase a video
+        System.out.print ("ciao a tutti");
+        System.out.println (" come state?");
+        System.out.println("aaaaa");
+        
+        //questo comando stampa dei numeri a video
+        System.out.println(123);
+        
+        /*
+        commentone
+        multirighe
+        guarda quante sono
+        hihihi
+        */
+    }
+    
+}
