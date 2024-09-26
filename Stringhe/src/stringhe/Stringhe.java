c3b1859a504ecc87749d7ed0d653412dd4bddc8e
Lorenzo Tripodi <157422066+Eltymus@users.noreply.github.com>
2024-09-26 14:06:59 +0200

    Add files via upload

---
+++ Stringhe/src/stringhe/Stringhe.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package stringhe;
+
+/**
+ *
+ * @author loren
+ */
+public class Stringhe {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String str="Java è un bel linguaggio di programmazione";
+        
+        String linguaggio=str.substring(14,24);
+        System.out.println(linguaggio);
+        
+        String last=str.substring(41);
+        System.out.println(last);
+        
+        String Frase=str.substring(14);
+        System.out.println(Frase);
+        
+        
+        
+        
+        
+        
+        
+        // TODO code application logic here
+    }
+    
+}
