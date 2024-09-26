1776dc2863ea991f64af270413aa71546890e1dd
Lorenzo Tripodi <157422066+Eltymus@users.noreply.github.com>
2024-09-26 14:05:32 +0200

    Add files via upload

---
+++ Ripassoeinizializzazione17/src/ripassoeinizializzazione17/Ripassoeinizializzazione17.java
@@ -0,0 +1,19 @@
+
+package ripassoeinizializzazione17;
+
+import java.util.Scanner;
+
+public class Ripassoeinizializzazione17 {
+    
+
+
+    public static void main(String[] args) {
+       
+        Scanner input= new Scanner(System.in);
+ 
+        System.out.println("inserisci la tua età: ");
+        int età=input.nextInt();
+        
+    }
+    
+}
