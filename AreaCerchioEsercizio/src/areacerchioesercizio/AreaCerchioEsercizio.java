1306fd517913ac9d7936cfd7be0d53e2b770b418
Lorenzo Tripodi <157422066+Eltymus@users.noreply.github.com>
2024-09-26 11:15:40 +0200

    Add files via upload

---
+++ AreaCerchioEsercizio/src/areacerchioesercizio/AreaCerchioEsercizio.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package areacerchioesercizio;
+
+/**
+ *
+ * @author loren
+ */
+public class AreaCerchioEsercizio {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+    //fare un programmino che calcola l'area del cerchio (pigreco*r*r)
+    //definire una costante pigreco
+        System.out.println(Math.PI);
+        double PI=3.141592653589793;
+    //dichiarare una variabile raggio con valore a piacere
+    double r= 5;
+    //dichiarare una variabile area che contiene l'area del cerchio
+    double Area=PI*r*r;
+    //stampare tutte le variabili del programma
+        System.out.println(Area);
+    }
+    
+}
